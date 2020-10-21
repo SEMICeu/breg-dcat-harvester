46e5a4f21d623f63ce1d21f96f6ccca51c722b31
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2020-10-21 12:44:10 +0200

    Cache remote RDF graphs in Redis

--- api/breg_harvester/browser.py
+++ api/breg_harvester/browser.py
@@ -1,3 +1,4 @@
+import collections
 import logging
 import pprint
 
@@ -7,17 +8,22 @@ from rdflib.namespace import DCAT, DCTERMS, SKOS
 from rdflib.plugin import PluginException
 
 import breg_harvester.store
+from breg_harvester.jobs_queue import get_redis
 
 _logger = logging.getLogger(__name__)
 
 BLUEPRINT_NAME = "browser"
 blueprint = Blueprint(BLUEPRINT_NAME, __name__)
 
-_FORMATS = [None, "xml", "turtle", "nt", "json-ld"]
+_FORMAT_REDIS = "xml"
+
+LoadGraphResult = collections.namedtuple(
+    "LoadGraphResult",
+    ["graph", "cache_hit", "parsed"])
 
 
-def _graph_parse(graph, term, formats=None):
-    formats = formats or _FORMATS
+def _parse_graph(graph, term):
+    formats = [None, "xml", "turtle", "nt", "json-ld"]
 
     for frmt in formats:
         try:
@@ -27,24 +33,79 @@ def _graph_parse(graph, term, formats=None):
             pass
 
 
-def _term_to_dict(term, extended=True, label_lang="en"):
+def _build_redis_key(term):
+    return f"breg:harvester:cache:{term.n3()}"
+
+
+def _set_graph_redis(graph, term, redis):
+    data = graph.serialize(destination=None, format=_FORMAT_REDIS)
+    name = _build_redis_key(term=term)
+    _logger.debug("SET %s", name)
+    redis.set(name=name, value=data)
+
+
+def _get_graph_redis(graph, term, redis):
+    name = _build_redis_key(term=term)
+    _logger.debug("GET %s", name)
+    data = redis.get(name=name)
+    assert data, f"Undefined key: {name}"
+    graph.parse(data=data, format=_FORMAT_REDIS)
+
+
+def _load_graph(term, redis, write_cache=True):
+    graph = Graph()
+    cache_hit = False
+    parsed = False
+
+    try:
+        _get_graph_redis(graph=graph, term=term, redis=redis)
+        cache_hit = True
+    except:
+        _logger.debug(
+            "Error loading term (%s) from Redis",
+            term, exc_info=True)
+
+    if not cache_hit:
+        try:
+            _parse_graph(graph=graph, term=term)
+            parsed = True
+        except:
+            _logger.debug(
+                "Error parsing term (%s)",
+                term, exc_info=True)
+
+    if write_cache and not cache_hit and parsed:
+        _set_graph_redis(graph=graph, term=term, redis=redis)
+
+    if not cache_hit and not parsed:
+        graph = None
+
+    res = LoadGraphResult(graph=graph, cache_hit=cache_hit, parsed=parsed)
+
+    _logger.debug("Load result for term %s: %s", term, res)
+
+    return res
+
+
+def _term_to_dict(term, redis, extended=True, label_lang="en"):
     ret = {"n3": term.n3()}
 
     if not extended:
         return ret
 
-    try:
-        graph = Graph()
-        _graph_parse(graph=graph, term=term)
-        labels = graph.preferredLabel(subject=term, lang=label_lang)
-        pref_label = labels[0] if len(labels) > 0 else None
+    load_res = _load_graph(term=term, redis=redis, write_cache=True)
 
-        ret.update({
-            "label": pref_label[1] if pref_label else None,
-            "label_prop": pref_label[0] if pref_label else None
-        })
-    except Exception as ex:
-        _logger.info("Error parsing term (%s): %s", term, ex)
+    if not load_res.graph:
+        return ret
+
+    labels = load_res.graph.preferredLabel(subject=term, lang=label_lang)
+    label = labels[0][1] if len(labels) > 0 else None
+    label_prop = labels[0][0] if len(labels) > 0 else None
+
+    ret.update({
+        "label": label,
+        "label_prop": label_prop
+    })
 
     return ret
 
@@ -66,8 +127,12 @@ def get_catalog_taxonomies():
 
     terms = set(item[2] for item in qres)
     extended = bool(request.args.get("ext"))
+    redis = get_redis()
 
-    return jsonify([_term_to_dict(term, extended=extended) for term in terms])
+    return jsonify([
+        _term_to_dict(term, redis=redis, extended=extended)
+        for term in terms
+    ])
 
 
 @blueprint.route("/dataset/theme", methods=["GET"])
@@ -87,5 +152,9 @@ def get_dataset_themes():
 
     terms = set(item[2] for item in qres)
     extended = bool(request.args.get("ext"))
+    redis = get_redis()
 
-    return jsonify([_term_to_dict(term, extended=extended) for term in terms])
+    return jsonify([
+        _term_to_dict(term, redis=redis, extended=extended)
+        for term in terms
+    ])
